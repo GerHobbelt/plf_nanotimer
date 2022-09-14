f9a1dc46b10930b056148142d850425463c3b816
Ger Hobbelt <ger@hobbelt.com>
2022-09-14 20:18:19 +0200

    - making sure the load-only-once boundary check does not clash with the C++ version of this library: they are independent. - copied the C++ implementation across for the other platforms, so now the C version also supports all (Linux/Windows/Android) - completed the C interface with an nanotimer_is_initialized() API so we can check whether we need to set up the timer (or equivalently: whether the timer is already 'running')

--- plf_nanotimer_c_api.h
+++ plf_nanotimer_c_api.h
@@ -17,8 +17,8 @@
 // 3. This notice may not be removed or altered from any source distribution.
 
 
-#ifndef PLF_NANOTIMER_H
-#define PLF_NANOTIMER_H
+#ifndef PLF_C_NANOTIMER_H
+#define PLF_C_NANOTIMER_H
 
 
 // ~Nanosecond-precision cross-platform (linux/bsd/mac/windows, C++03/C++11) simple timer class:
@@ -28,89 +28,114 @@
 #include <mach/clock.h>
 #include <mach/mach.h>
 
-namespace plf
+typedef struct plf_nanotimer_data
 {
+	clock_serv_t system_clock;
+	mach_timespec_t time1, time2;
+	int is_init;
+} nanotimer_data_t;
 
-	class nanotimer
+static inline void nanotimer(nanotimer_data_t *store_ref)
+{
+	host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &store_ref->system_clock);
+	store_ref->is_init = 1;
+}
+static inline void nanotimer_destroy(nanotimer_data_t *store_ref)
+{
+	mach_port_deallocate(mach_task_self(), &store_ref->system_clock);
+	store_ref->is_init = 0;
+}
+static inline void nanotimer_is_initialized(nanotimer_data_t *store_ref)
+{
+	return store_ref->is_init;
+}
+
+static inline void nanotimer_start(nanotimer_data_t *store_ref)
+{
+	if (!store_ref->is_init)
 	{
-	private:
-		clock_serv_t system_clock;
-		mach_timespec_t time1, time2;
-	public:
-		nanotimer()
-		{
-			host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &system_clock);
-		}
+		// code hasn't executed the initializer yet.
+		nanotimer(store_ref);
+	}
+	clock_get_time(store_ref->system_clock, &store_ref->time1);
+}
 
-		~nanotimer()
-		{
-			mach_port_deallocate(mach_task_self(), system_clock);
-		}
+static inline double nanotimer_get_elapsed_ns(nanotimer_data_t *store_ref)
+{
+	if (!store_ref->is_init)
+	{
+		// code hasn't executed the initializer yet. Nor did it call nanotimer_start() earlier to start the measurement.
+		return -1;
+	}
+	clock_get_time(store_ref->system_clock, &store_ref->time2);
+	return ((1000000000.0 * (double)(store_ref->time2.tv_sec - store_ref->time1.tv_sec)) + (double)(store_ref->time2.tv_nsec - store_ref->time1.tv_nsec));
+}
 
-		inline void start() PLF_NOEXCEPT
-		{
-			clock_get_time(system_clock, &time1);
-		}
+static inline double nanotimer_get_elapsed_us(nanotimer_data_t *store_ref)
+{
+	return nanotimer_get_elapsed_ns(store_ref) / 1000.0;
+}
 
-		inline double get_elapsed_ms() PLF_NOEXCEPT
-		{
-			return static_cast<double>(get_elapsed_ns()) / 1000000.0;
-		}
-
-		inline double get_elapsed_us() PLF_NOEXCEPT
-		{
-			return static_cast<double>(get_elapsed_ns()) / 1000.0;
-		}
-
-		double get_elapsed_ns() PLF_NOEXCEPT
-		{
-			clock_get_time(system_clock, &time2);
-			return ((1000000000.0 * static_cast<double>(time2.tv_sec - time1.tv_sec)) + static_cast<double>(time2.tv_nsec - time1.tv_nsec));
-		}
-	};
-
-
-
+static inline double nanotimer_get_elapsed_ns(nanotimer_data_t *store_ref)
+{
+	return nanotimer_get_elapsed_ns(store_ref) / 1000000.0;
+}
 
 	// Linux/BSD implementation:
 #elif (defined(linux) || defined(__linux__) || defined(__linux)) || (defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__))
 #include <time.h>
 #include <sys/time.h>
 
-namespace plf
+typedef struct plf_nanotimer_data
 {
+	struct timespec time1, time2;
+	int is_init;
+} nanotimer_data_t;
 
-	class nanotimer
+static inline void nanotimer(nanotimer_data_t *store_ref)
+{
+	clock_gettime(CLOCK_MONOTONIC, &store_ref->time1);
+	store_ref->is_init = 1;
+}
+static inline void nanotimer_destroy(nanotimer_data_t *store_ref)
+{
+	store_ref->is_init = 0;
+}
+static inline void nanotimer_is_initialized(nanotimer_data_t *store_ref)
+{
+	return store_ref->is_init;
+}
+
+static inline void nanotimer_start(nanotimer_data_t *store_ref)
+{
+	if (!store_ref->is_init)
 	{
-	private:
-		struct timespec time1, time2;
-	public:
-		nanotimer() PLF_NOEXCEPT {}
+		// code hasn't executed the initializer yet.
+		nanotimer(store_ref);
+	}
+	clock_get_time(store_ref->system_clock, &store_ref->time1);
+}
 
-		inline void start() PLF_NOEXCEPT
-		{
-			clock_gettime(CLOCK_MONOTONIC, &time1);
-		}
+static inline double nanotimer_get_elapsed_ns(nanotimer_data_t *store_ref)
+{
+	if (!store_ref->is_init)
+	{
+		// code hasn't executed the initializer yet. Nor did it call nanotimer_start() earlier to start the measurement.
+		return -1;
+	}
+	clock_gettime(CLOCK_MONOTONIC, &store_ref->time2);
+	return ((1000000000.0 * (double)(store_ref->time2.tv_sec - store_ref->time1.tv_sec)) + (double)(store_ref->time2.tv_nsec - store_ref->time1.tv_nsec));
+}
 
-		inline double get_elapsed_ms() PLF_NOEXCEPT
-		{
-			return get_elapsed_ns() / 1000000.0;
-		}
+static inline double nanotimer_get_elapsed_us(nanotimer_data_t *store_ref)
+{
+	return nanotimer_get_elapsed_ns(store_ref) / 1000.0;
+}
 
-		inline double get_elapsed_us() PLF_NOEXCEPT
-		{
-			return get_elapsed_ns() / 1000.0;
-		}
-
-		double get_elapsed_ns() PLF_NOEXCEPT
-		{
-			clock_gettime(CLOCK_MONOTONIC, &time2);
-			return ((1000000000.0 * static_cast<double>(time2.tv_sec - time1.tv_sec)) + static_cast<double>(time2.tv_nsec - time1.tv_nsec));
-		}
-	};
-
-
-
+static inline double nanotimer_get_elapsed_ns(nanotimer_data_t *store_ref)
+{
+	return nanotimer_get_elapsed_ns(store_ref) / 1000000.0;
+}
 
 	// Windows implementation:
 #elif defined(_WIN32)
@@ -119,8 +144,8 @@ namespace plf
 
 typedef struct plf_nanotimer_data
 {
-	LARGE_INTEGER ticks1, ticks2;
 	double frequency;
+	LARGE_INTEGER ticks1, ticks2;
 } nanotimer_data_t;
 
 static inline void nanotimer(nanotimer_data_t *store_ref)
@@ -128,6 +153,14 @@ static inline void nanotimer(nanotimer_data_t *store_ref)
 	LARGE_INTEGER freq;
 	QueryPerformanceFrequency(&freq);
 	store_ref->frequency = (double)freq.QuadPart;
+}
+static inline void nanotimer_destroy(nanotimer_data_t *store_ref)
+{
+	store_ref->frequency = 0;
+}
+static inline int nanotimer_is_initialized(nanotimer_data_t *store_ref)
+{
+	return !!store_ref->frequency;
 }
 
 static inline void nanotimer_start(nanotimer_data_t *store_ref)
@@ -161,9 +194,9 @@ static inline double nanotimer_get_elapsed_ns(nanotimer_data_t *store_ref)
 	return nanotimer_get_elapsed_ms(store_ref) * 1000000.0;
 }
 
+#else
+#error "failure warning - your OS is not supported"
 #endif
-	// Else: failure warning - your OS is not supported
-
 
 
 #if defined(__MACH__) || (defined(linux) || defined(__linux__) || defined(__linux)) || (defined(__DragonFly__) || defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)) || defined(_WIN32)
